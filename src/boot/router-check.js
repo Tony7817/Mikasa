33401ce128cd18439b9c880e3dd4df5547512271
Tony <cwlhahahaha@163.com>
2025-03-30 23:52:06 +0800

    fix bug of login

--- src/boot/router-check.js
+++ src/boot/router-check.js
@@ -3,6 +3,19 @@ import { useUserStore } from "src/stores/user";
 import { Loading } from "quasar";
 import { jwtDecode } from "jwt-decode";
 
+function isAuthenticated() {
+  const userStore = useUserStore();
+  if (!userStore.user?.token) {
+    return false;
+  }
+  const token = jwtDecode(userStore.user.token);
+  const currentTime = Date.now() / 1000;
+  if (token.exp < currentTime) {
+    userStore.clearUser();
+    return false;
+  }
+  return true;
+}
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async ({ app, router }) => {
@@ -10,16 +23,18 @@ export default boot(async ({ app, router }) => {
     Loading.show({
       message: "Loading...",
     });
-    const userStore = useUserStore();
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-      if (!userStore.user?.token) {
-        next({ name: "login", query: { next: to.fullPath } });
+    const isAuth = isAuthenticated();
+    if (to.name === "login") {
+      if (isAuth) {
+        next({ name: "home" });
+      } else {
+        next();
       }
-      const token = jwtDecode(userStore.user.token);
-      const currentTime = Date.now() / 1000;
-      if (token.exp < currentTime) {
-        userStore.clearUser();
+    } else if (to.matched.some((record) => record.meta.requiresAuth)) {
+      if (!isAuth) {
         next({ name: "login", query: { next: to.fullPath } });
+      } else {
+        next();
       }
     } else {
       next();
